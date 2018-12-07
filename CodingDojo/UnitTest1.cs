286758b8d6bd5cf15ca269411f17c7e68636a068
Jörg Weißbecker <j.weissbecker@cid.com>
2018-12-07 12:42:37 +0100

    created Project

---
+++ CodingDojo/UnitTest1.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace CodingDojo
+{
+    public class Implementation
+    {
+
+    }
+
+
+    public class Tests
+    {
+        
+        [Test]
+        public void Constructor_Works()
+        {
+            new Implementation();
+            Assert.Pass();
+        }
+    }
+}
